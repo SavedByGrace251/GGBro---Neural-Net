627999c3a8b5844926cfda89bc4c40ab797112da
Jonathan Newell <SavedByGrace251@gmail.com>
2016-02-13 16:33:02 -0900

    pushing test stuff

--- GGBro/main.cpp
+++ GGBro/main.cpp
@@ -22,25 +22,39 @@ int main() {
 
     using namespace chrono;
     
-    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    high_resolution_clock::time_point t1;
+    high_resolution_clock::time_point t2;
+	duration<double> timeSpan;
     
-	Board init("_____________bb__R___bb_________", false);
-    
-    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-    
-    duration<double> time_span1 = duration_cast<duration<double>>(t2 - t1);
-    cout << "Board contruction took " << time_span1.count() << " seconds." << endl;
-    
-	init.redTurn = true;
     t1 = high_resolution_clock::now();
     int count = 0;
-    
-    while (time_span1.count() < 10) {
-        vector<Board> newBoards = init.generateLegalMoves();
-        t2 = high_resolution_clock::now();
-        time_span1 = duration_cast<duration<double>>(t2 - t1);
+	vector<int> layers{ 32, 40, 10, 1 };
+	NeuralNetwork net(layers);
+	vector<double> weights(net.numberOfWeights);
+	uniform_real_distribution<double> uniform(0, 1);
+	default_random_engine engine;
+
+	for (auto & weight : weights) {
+		weight = uniform(engine);
+
+	}
+	vector<double> inputs(33);
+	for (auto & weight : inputs) {
+		weight = uniform(engine);
+
+	}
+
+	net.setWeights(weights);
+	net.setInput(inputs);
+
+	cout << net.Activate()[0] << endl;
+
+    /*while (count <= 1000000) {
+		net.Activate();
         count++;
-    }
+    }*/
+	t2 = high_resolution_clock::now();
+	timeSpan = duration_cast<duration<double>>(t2 - t1);
     
     cout << "Board generations: " << count << "." << endl;
     return 0;
