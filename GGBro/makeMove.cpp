16c28eb7885c761f4161fc214ad14cc8e4c03ed0
Jonathan Newell <SavedByGrace251@gmail.com>
2016-04-14 17:10:16 -0800

    Added makeMove executable

---
+++ GGBro/makeMove.cpp
@@ -0,0 +1,36 @@
+//	Authors: Jay Byam, Jonathan Newell
+//
+//  TopCompete.cpp
+//  GGBro
+//
+
+#include "Board.h"
+#include "Piece.h"
+#include "AI.h"
+#include "Checkers.h"
+#include "Tournament.h"
+#include "Training.h"
+#include <fstream>
+#include <iterator>
+using namespace std;
+
+int main(int nArgs, char* args[]) {
+	
+	if (nArgs < 2) {
+		return 666;
+	}
+	
+	string fileLoc = "player.data";
+	string board = args[1];
+	string team = args[2];
+	bool isRed  = team.compare("red");
+	
+	ifstream playerFile("fileLoc");
+	
+	Board b(board);
+	AI player;
+	playerFile >> player;
+	player.playAsRed = isRed;
+	cout << player.makeMove(b).toString() << endl;
+	
+}
