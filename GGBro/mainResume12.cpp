9a717883e528750708e4082087fde1963be04737
Jonathan Newell <SavedByGrace251@gmail.com>
2016-04-19 16:05:37 -0800

    added additional job script for 12 and 96 hour run times, renamed main files for same reasons

---
+++ GGBro/mainResume12.cpp
@@ -0,0 +1,34 @@
+//	Authors: Jay Byam, Jonathan Newell
+//
+//  main.cpp
+//  GGBro
+//
+
+#include "Board.h"
+#include "Piece.h"
+#include "AI.h"
+#include "Checkers.h"
+#include "Tournament.h"
+#include "Training.h"
+#include <fstream>
+#include <iterator>
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	cout << "Training Started" << endl;
+	Training t;
+	t.resume();
+	t.setMaxTime(0, 0, 11, 57, 0);
+	t.train();
+	cout << "Training Ended" << endl;
+
+	return 0;
+}
